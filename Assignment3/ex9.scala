d3ad32ae60f31c66bf6f4af0f17c31d89b69646a
Saad Ali <59177839+SaadAliHafiz@users.noreply.github.com>
2021-10-04 00:23:47 -0700

    Add files via upload

---
+++ Assignment3/ex9.scala
@@ -0,0 +1,13 @@
+package Asssignment3
+
+object ex9  {
+
+  def main(args:Array[String]) {
+    println(appl(List(1, 2, 4, 5)))
+  }
+  def appl(lst:List[Int]): Int ={
+    var x=lst.sum
+//    var y=lst
+     x
+  }
+}
